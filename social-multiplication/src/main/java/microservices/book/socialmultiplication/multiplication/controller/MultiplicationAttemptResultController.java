d0fc6f622a90b91fbc4dae8a3f114428e6254c3b
Nick Fernando <nfernan1@uci.edu>
2018-10-12 06:57:08 -0700

    Add AttemptResultController and test. Inner class has some issues still.

--- social-multiplication/src/main/java/microservices/book/socialmultiplication/multiplication/controller/MultiplicationAttemptResultController.java
+++ social-multiplication/src/main/java/microservices/book/socialmultiplication/multiplication/controller/MultiplicationAttemptResultController.java
@@ -1,10 +1,11 @@
 package microservices.book.socialmultiplication.multiplication.controller;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import microservices.book.socialmultiplication.multiplication.domain.MultiplicationResultAttempt;
 import microservices.book.socialmultiplication.multiplication.service.MultiplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,11 +19,17 @@ public final class MultiplicationAttemptResultController {
     public MultiplicationAttemptResultController(MultiplicationService multiplicationService) {
         this.multiplicationService = multiplicationService;
     }
-
-    @RequiredArgsConstructor
-    @NoArgsConstructor(force = true)
-    @Getter
-    public static final class ResultResponse {
+    private static final class ResultResponse {
         private final boolean correct;
+        public ResultResponse(boolean correct) {
+            this.correct = correct;
+        }
+    }
+
+    @PostMapping
+    ResponseEntity<ResultResponse> postResult(@RequestBody MultiplicationResultAttempt multiplicationResultAttempt) {
+        return ResponseEntity.ok(
+                new ResultResponse(multiplicationService
+                        .checkAttempt(multiplicationResultAttempt)));
     }
 }
