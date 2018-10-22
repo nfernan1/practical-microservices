767d083bc708c12cff4deaf68c15e922737fd0d2
Nick Fernando <nfernan1@uci.edu>
2018-10-22 08:58:00 -0700

    Add repo and hibernate JPA layer

--- social-multiplication/src/main/java/microservices/book/socialmultiplication/multiplication/domain/Multiplication.java
+++ social-multiplication/src/main/java/microservices/book/socialmultiplication/multiplication/domain/Multiplication.java
@@ -2,28 +2,30 @@ package microservices.book.socialmultiplication.multiplication.domain;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@RequiredArgsConstructor
 @Getter
 @ToString
 @EqualsAndHashCode
+@Entity
 public final class Multiplication {
-    private int factorA = 0;
-    private int factorB = 0;
 
-    public Multiplication() {
-    }
+    @Id
+    @GeneratedValue
+    @Column(name="MULTIPLICATION_ID")
+    private Long id;
 
-    public Multiplication(int factorA, int factorB) {
-        this.factorA = factorA;
-        this.factorB = factorB;
-    }
+    private final int factorA;
+    private final int factorB;
 
-    public int getFactorA() {
-        return factorA;
-    }
-
-    public int getFactorB() {
-        return factorB;
+    Multiplication() {
+        this(0, 0);
     }
 }
